66db147f7f4c64dae35eb2ef86999783c0c59285
Eric Drechsel <eric@pointnineseven.com>
2014-08-13 16:40:59 -0700

    OST-195 intermediate step in refactoring PU layer to render fresh on filter

--- server/static/survey/assets/js/directives/dash-map-ost.js
+++ server/static/survey/assets/js/directives/dash-map-ost.js
@@ -21,7 +21,8 @@ angular.module('askApp').directive('dashMapOst', function($http, $compile, $time
     directive.link = function (scope, element) {
         var map,
             markers = [],
-            controls;
+            controls,
+            puPromise;
 
         function init () {
             map = MapUtils.initMap(element[0].children[0].children[0],
@@ -31,15 +32,17 @@ angular.module('askApp').directive('dashMapOst', function($http, $compile, $time
                 map.addLayer(layer)
                 map.controls.addOverlay(layer, 'Boundary');
             });
-            
-            MapUtils.addPlanningUnitGrid("/static/survey/data/CentralCalifornia_PlanningUnits.json", map, function (layer) {
-                scope.puLayer = layer;
-                map.addLayer(scope.puLayer)
-                map.controls.addOverlay(scope.puLayer, 'Planning Units');
 
-                scope.$watch('units', updatePuLayer);
+            puPromise = $http.get("/static/survey/data/CentralCalifornia_PlanningUnits.json")
+            scope.$watch('units', updatePuLayer);
 
-            });
+            scope.puLayer = L.geoJson();
+            scope.puLayer.setStyle({
+                "fillOpacity": 0,
+                "stroke": false
+            })
+            map.addLayer(scope.puLayer);
+            map.controls.addOverlay(scope.puLayer, 'Planning Units');
 
             scope.markersLayer = L.featureGroup();
             map.addLayer(scope.markersLayer);
@@ -125,21 +128,18 @@ angular.module('askApp').directive('dashMapOst', function($http, $compile, $time
             });
         };
 
-        function updatePuLayer(units){
-            // Turns off all cells
-            _.each(scope.puLayer._layers, function(sublayer){
-                sublayer.setStyle({
-                    fillOpacity: 0,
-                    stroke: false // color: '#E6D845'
-                });
-            });
 
-            // Add opacity to active cells.
-            _.each(units, function(unit){
-                scope.setCellActive(unit);
+        function updatePuLayer(units){
+            puPromise.success(function(data) {
+                scope.puLayer.clearLayers();
+                scope.puLayer.addData(data);
+
+                _.each(units, function(unit){
+                    scope.setCellActive(unit);
+                });
+                scope.puLayer.bringToBack();
             });
         };
-
 
         function setPuPopup (layer){
             // Sets a pop for this planning unit.
@@ -323,31 +323,6 @@ angular.module('askApp').directive('dashMapOst', function($http, $compile, $time
             }
         },
 
-        addPlanningUnitGrid: function (geojsonPath, map, success_callback) {
-            // Add planning units grid with no borders from /static/survey/data/CentralCalifornia_PlanningUnits.json
-            $http.get(geojsonPath).success(function(data) {
-                var geojsonLayer = L.geoJson(data, { 
-                    style: function(feature) {
-                        return {
-                            "color": "#E6D845",
-                            "weight": 1,
-                            "opacity": 0.6,
-                            "fillOpacity": 0.0
-                        };
-                    },
-                    onEachFeature: function(feature, layer) {
-                        var id = layer.feature.properties.ID
-                        layer.on("dblclick", function (e) {
-                            map.setZoom(map.getZoom() + 1);
-                            console.log("Click layer "+id);
-                            console.log(e)
-                        });
-                    }
-                });
-                success_callback(geojsonLayer);
-            });
-        },
-        
         createMarker: function (config) {
             var marker = null;
             if (config.lat && config.lat) {
