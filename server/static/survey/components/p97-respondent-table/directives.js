point97/survey-ost

commit 79014838cb4daf6809145c7c25755c02af136688

OST-302 eliminate duplicate API reqs
2x collection-areas/points
2x monitoring-project
3x dashrespondant

All due to various ng watches.

server/static/survey/components/p97-respondent-table/directives.js
@@ -142,16 +142,11 @@ angular.module('askApp')
             };
 
 
-            // Load data when the resource is defined.
-            scope.$watch('resource', function(newVal){
-                if (newVal && scope.respondents === null){
-                    scope.goToPage(1);
-                }
-            });
-
             // Watch the params to see if ef changed
             scope.$watch('params.ef', function(newVal){
-                scope.goToPage(1);
+                if (newVal) {
+                    scope.goToPage(1);
+                }
 
             });
 

server/static/survey/assets/js/controllers/ost/dash-overview.js
@@ -9,7 +9,7 @@ angular.module('askApp').controller('DashOverviewCtrl', function($scope, $http, 
 
 
         $scope.filtersJson = '';
-        $scope.filters = { ecosystemFeatures: [] };
+        $scope.filters = {};
         
         // Setup respondent table params and options
         var complete = ($scope.user.is_staff !== true)
@@ -36,20 +36,15 @@ angular.module('askApp').controller('DashOverviewCtrl', function($scope, $http, 
             lng: -122.0000000,
             zoom: 7
         };
-        $scope.updateMap();
 
         $scope.$watch('filters.ecosystemFeatures', function(newVal, oldVal) {
             
             // Update $scope.respondentTable so it reloads with new filters in place
-            $scope.respondentTable.params.ef = $scope.filters.ecosystemFeatures;
+            if (newVal) {
+                $scope.respondentTable.params.ef = newVal;
+                $scope.updateMap();
+            }
 
-
-            // Not sure where this is used
-            $scope.filtersJson = [];
-            _.each($scope.filters.ecosystemFeatures, function (label) {
-                var slug = ecosystemLabelToSlug(label);
-                //$scope.filtersJson.push({'ecosystem-features': slug});
-            });
 
         });
     }

server/static/survey/assets/js/directives/dash-filter.js
@@ -60,14 +60,13 @@ angular.module('askApp')
                 dashData.getDistribution(scope.surveySlug, scope.questionSlug,
                     '' /*no filter*/, onSuccess, onFail);
             }
-            scope.selectionChanged = function (value) {
-                scope.selectedValues = scope.model.selectedValuesInternal;
+            scope.$watch('model.selectedValuesInternal', function(newVal, oldVal) {
+                scope.selectedValues = newVal;
                 if (scope.$parent.filters && scope.$parent.updateMap){
                     scope.$parent.filters.ecosystemFeatures = scope.selectedValues;    
-                    scope.$parent.updateMap();
                 }
                 
-            };
+            });
 
 
             setFilterOptions();

commit 06e0341b0959abad7e637811c13c2bd400196741

fix search breakage introduced in 79014838c

server/static/survey/components/p97-respondent-table/directives.js
@@ -144,7 +144,12 @@ angular.module('askApp')
                 url = url.join('');
                 return url;
             };
-
+            // Load data when the resource is defined.
+            scope.$watch('resource', function(newVal){
+                if (newVal && scope.respondents === null){
+                    scope.goToPage(1);
+                }
+            });
 
             // Watch the params to see if ef changed
             scope.$watch('params.ef', function(newVal){