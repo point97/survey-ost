9d3750a928c8772db79ceb738d5bf7ec8fdebe1d
Drew <drew@pointnineseven.com>
2015-09-09 10:46:02 -0700

    display points for both survey

--- server/static/survey/assets/js/controllers/ost/dash-overview.js
+++ server/static/survey/assets/js/controllers/ost/dash-overview.js
@@ -1,5 +1,5 @@
 
-angular.module('askApp').controller('DashOverviewCtrl', function($scope, $rootScope, $http, $routeParams, $location, dashData, chartUtils, survey) {
+angular.module('askApp').controller('DashOverviewCtrl', function($scope, $rootScope, $http, $routeParams, $location, $q, dashData, chartUtils, survey) {
 
     $scope.page_title = "Monitoring Activities";
     $scope.loadingSurveys = true;
@@ -37,6 +37,7 @@ angular.module('askApp').controller('DashOverviewCtrl', function($scope, $rootSc
     //
     // Map
     // 
+
     $scope.updateMap = function (action) {
         /*
         Params:
@@ -58,32 +59,72 @@ angular.module('askApp').controller('DashOverviewCtrl', function($scope, $rootSc
         var filtersJson = _.map($rootScope.filters.ecosystemFeatures, function (label) {
             var slug = ecosystemLabelToSlug(label);
             if (slug.length>0) {
-                return {'ncc-ecosystem-features': slug};
+                return {'ecosystem-features': slug};
             } else {
                 return null;
             }
         });
+
         filtersJson = _.flatten(filtersJson);
 
-        var pointsUrl = pointsApiUrl('ncc-monitoring', '*-collection-points', filtersJson),
-            polysUrl = polysApiUrl('ncc-monitoring', '*-collection-areas', filtersJson);
+        var nccPointsUrl = pointsApiUrl('ncc-monitoring', '*-collection-points', filtersJson),
+            ccPointsUrl = pointsApiUrl('monitoring-project', '*-collection-points', filtersJson),
+            nccPolysUrl = polysApiUrl('ncc-monitoring', '*-collection-areas', filtersJson),
+            ccPolysUrl = polysApiUrl('monitoring-project', '*-collection-areas', filtersJson);
         
-        $scope.activeEcosystemFeatures = _.pluck(filtersJson, 'ncc-ecosystem-features')
-        getPoints(pointsUrl, function (points) {
-            $scope.mapSettings.mapPoints = points;
-            var uniq = [];
-            _.each(points, function (point) {
-                if (! _.contains(uniq, point.qSlug)) {
-                    uniq.push(point.qSlug);
+        $scope.activeEcosystemFeatures = _.pluck(filtersJson, 'ncc-ecosystem-features');
+
+        $scope.ccMapPoints = [];
+        $scope.nccMapPoints = [];
+        var nccPoints = $http.get(nccPointsUrl);
+        var ccPoints = $http.get(ccPointsUrl);
+        var nccPolys = $http.get(nccPolysUrl);
+        var ccPolys = $http.get(ccPolysUrl);
+
+        $q.all([nccPoints, ccPoints, nccPolys, ccPolys]).then(function(data) {
+            $scope.ccMapPoints = processGeojson(data[0].data);
+            $scope.nccMapPoints = processGeojson(data[1].data);
+            // $scope.mapSettings.mapPlanningUnits = data[3].data.answers;
+            var uniqNCC = [];
+            var uniqCC = [];
+
+            $scope.uniqueCCSlugs = isUniq(data[0], uniqCC)
+            $scope.uniqueNCCSlugs = isUniq(data[1], uniqNCC)
+            $scope.mapSettings.mapPoints = $scope.ccMapPoints.concat($scope.nccMapPoints);
+
+        });
+    };
+
+    function processGeojson(surveyData) {
+        var points = [];
+        _.each(surveyData.geojson, function (item) {
+            if (item.geojson) {
+                var feature = JSON.parse(item.geojson)
+                  , lat = feature.geometry.coordinates[1]
+                  , lng = feature.geometry.coordinates[0]
+                  , uuid = feature.properties.activity
+                  , qSlug = feature.properties.label
+                  ;
+                if (lat && lng && uuid && qSlug) {
+                    points.push({
+                        lat: lat,
+                        lng: lng,
+                        uuid: uuid,
+                        qSlug: qSlug});
                 }
-            });
-            $scope.uniqueEcosystemFeatureSlugs = uniq;
+            };
+            
         });
-    
-        getPolys(polysUrl, function (polys) {
-            $scope.mapSettings.mapPlanningUnits = polys;
+        return points;
+    };
+
+    function isUniq(surveyData, surveyArray) {
+        _.each(surveyData, function (point) {
+            if (! _.contains(surveyArray, point.qSlug)) {
+                surveyArray.push(point.qSlug);
+            }
         });
-    }
+    };
 
     function pointsApiUrl (sSlug, qSlug, filtersJson) {
         var url = ['/reports/geojson', sSlug, qSlug];
@@ -99,42 +140,8 @@ angular.module('askApp').controller('DashOverviewCtrl', function($scope, $rootSc
             url.push('?filters=' + JSON.stringify(filtersJson));
         }
         return url.join('/');
-    }
+    };
     
-    function getPoints (url, success_callback) {
-        $http.get(url).success(function(data) {
-            // Set points collection (bound to directive)
-            var points = [];
-            _.each(data.geojson, function (item) {
-                if (item.geojson) {
-                    var feature = JSON.parse(item.geojson)
-                      , lat = feature.geometry.coordinates[1]
-                      , lng = feature.geometry.coordinates[0]
-                      , uuid = feature.properties.activity
-                      , qSlug = feature.properties.label
-                      ;
-                    if (lat && lng && uuid && qSlug) {
-                        points.push({
-                            lat: lat,
-                            lng: lng,
-                            uuid: uuid,
-                            qSlug: qSlug});
-                    }
-                };
-                
-            });
-
-            success_callback(points);
-        });
-    }
-
-
-    function getPolys (url, success_callback) {
-        $http.get(url).success(function(data) {
-            success_callback(data.answers);
-        });
-    }
-
     function ecosystemLabelToSlug (label) {
         return survey.ecosystemLabelToSlug(label);
     }
